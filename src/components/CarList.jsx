eaad9c64f0c23354769d4c0243c05a57a988c71f
s.alvarezinver <s.alvarezinver@gmail.com>
2025-11-17 20:39:59 +0000

    quedo bello responsive caritem

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -11,9 +11,11 @@ const CarList = () => {
         <h2>¡Excelente noticia, [Nombre]!</h2>
         <p>Tu crédito fue preaprobado por [$$Monto]. Elige tu vehículo y revisa los planes que aplican para ti.</p>
       </div>
-      {cars.map((car, index) => (
-        <CarItem key={index} car={car} />
-      ))}
+      <div className="car-grid">
+        {cars.map((car, index) => (
+          <CarItem key={index} car={car} />
+        ))}
+      </div>
     </div>
   );
 };
